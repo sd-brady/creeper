2c887121c00ce58cbef5d35c9ed8467d60b8fb3b
brady-respec <brady.mills@respec.com>
2023-07-01 08:28:33 -0600

    test commit

--- src/run.py
+++ src/run.py
@@ -3,6 +3,8 @@ from PyQt5 import QtWidgets as qtw
 
 from frontend import controller
 
+## THIS IS A TEST COMMENT
+
 if __name__ == "__main__":
     app = qtw.QApplication(sys.argv)
     mw = controller.MainWindow()
